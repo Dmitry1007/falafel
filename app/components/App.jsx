0223b8d362d7016a652ca9e8fdbdbcc2a0586c52
Dmitry Sky <dmitry.sky@icloud.com>
2016-05-01 10:22:16 -0600

    define an addNote click event handler on the app object

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -3,27 +3,46 @@ import React from 'react';
 
 
 export default class App extends React.Component {
+  constructor(props) {
+    console.log(props)
+    super(props);
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry'
+        }
+      ]
+    };
+  }
+
   render() {
-    const notes = [
-      {
-        id: uuid.v4(),
-        task: 'Learn Webpack'
-      },
-      {
-        id: uuid.v4(),
-        task: 'Learn React'
-      },
-      {
-        id: uuid.v4(),
-        task: 'Do laundry'
-      }
-    ];
+    const notes = this.state.notes;
     return (
       <div>
+        <button onClick={this.addNote}>+</button>
         <ul>{notes.map(note =>
           <li key={note.id}>{note.task}</li>
         )}</ul>
       </div>
     );
   }
+
+  addNote = () => {
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New task'
+      }])
+    });
+  };
 }
