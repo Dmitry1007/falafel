394d44e691383fff378f28ac7e36f2902cc08203
Dmitry Sky <dmitry.sky@icloud.com>
2016-05-01 11:16:35 -0600

    edit the task state of a note

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,5 +1,63 @@
 import React from 'react';
 
-// export default (props) => <div>{props.task}</div>;
-// OR destructuring
-export default ({task}) => <div>{task}</div>;
+export default class Note extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      editing: false
+    };
+  }
+
+  render() {
+    if(this.state.editing) {
+      return this.renderEdit();
+    }
+    return this.renderNote();
+  }
+
+  renderEdit = () => {
+    return <input type="text"
+      ref={
+        element => element ?
+        element.selectionStart = this.props.task.length :
+        null
+      }
+      autoFocus={true}
+      defaultValue={this.props.task}
+      onBlur={this.finishEdit}
+      onKeyPress={this.checkEnter} />;
+  };
+
+  renderNote = () => {
+    return <div onClick={this.edit}>{this.props.task}</div>;
+  };
+
+  edit = () => {
+    this.setState({
+      editing: true
+    });
+  };
+
+  checkEnter = (e) => {
+    if(e.key === 'Enter') {
+      this.finishEdit(e);
+    }
+  };
+
+  finishEdit = (e) => {
+    // `Note` will trigger an optional `onEdit` callback once it
+    // has a new value. We will use this to communicate the change to
+    // `App`.
+
+    const value = e.target.value;
+
+    if(this.props.onEdit) {
+      this.props.onEdit(value);
+
+      this.setState({
+        editing: false
+      });
+    }
+  };
+}
